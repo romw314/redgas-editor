30e5275feaeddd65055e58fbdfd410c220a3afc6
romw314 <106016361+romw314@users.noreply.github.com>
2023-10-28 17:59:34 +0200

    Resize the app window from 800x600 to 1000x800

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function createWindow () {
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
 		autoHideMenuBar: true,
-		width: 800,
-		height: 600,
+		width: 1000,
+		height: 800,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 			sandbox: false
